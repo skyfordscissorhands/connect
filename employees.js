04ca6d029d9eb697883eb1ca3283f40ea9283aaa
skyfordscissorhands <71157519+skyfordscissorhands@users.noreply.github.com>
2026-04-21 12:11:51 -0600

    Update employees.js

--- employees.js
+++ employees.js
@@ -1,5 +1,280 @@
 const employees = {
+  "adam-arms": {
+    name: "Adam Arms",
+    title: "",
+    email: "adam@aei-corp.com",
+    phone: "(405) 401-6518"
+  },
+  "tricia-bennett": {
+    name: "Tricia Bennett",
+    title: "",
+    email: "tricia@aei-corp.com",
+    phone: "(405) 401-8836"
+  },
+  "chuck-clark": {
+    name: "Chuck Clark",
+    title: "",
+    email: "chuck@aei-corp.com",
+    phone: "(405) 831-0270"
+  },
+  "bob-clevenger": {
+    name: "Bob Clevenger",
+    title: "",
+    email: "bob.clevenger@aei-corp.com",
+    phone: "(928) 812-3258"
+  },
+  "joseph-datin": {
+    name: "Joseph Datin",
+    title: "",
+    email: "joseph@aei-corp.com",
+    phone: "(405) 831-3879"
+  },
+  "tim-dennis": {
+    name: "Tim Dennis",
+    title: "",
+    email: "bones@aei-corp.com",
+    phone: "(405) 831-9128"
+  },
+  "wes-foster": {
+    name: "Wes Foster",
+    title: "",
+    email: "wes@aei-corp.com",
+    phone: "(405) 306-8481"
+  },
+  "jenny-gowens": {
+    name: "Jenny Gowens",
+    title: "",
+    email: "jenny@aei-corp.com",
+    phone: "(405) 740-2055"
+  },
+  "bill-graham": {
+    name: "Bill Graham",
+    title: "",
+    email: "bill@aei-corp.com",
+    phone: "(405) 831-9143"
+  },
+  "natalie-johnson": {
+    name: "Natalie Johnson",
+    title: "AI Executive Assistant",
+    email: "natalie@aei-corp.com",
+    phone: "(801) 636-9699"
+  },
+  "robert-keyes": {
+    name: "Robert Keyes",
+    title: "CEO",
+    email: "robert@aei-corp.com",
+    phone: "(405) 831-9140"
+  },
+  "scott-tripp": {
+    name: "Scott Tripp",
+    title: "",
+    email: "scott.tripp@aei-corp.com",
+    phone: "(580) 471-4144"
+  },
+
+  "jeff-brammer": {
+    name: "Jeff Brammer",
+    title: "ST Operations Team Leader",
+    email: "jeff.brammer@ecovacservices.com",
+    phone: "(405) 532-2766"
+  },
+  "robbie-mosley": {
+    name: "Robbie Mosley",
+    title: "",
+    email: "robbie.mosley@ecovacservices.com",
+    phone: "(405) 317-3026"
+  },
+  "corey-walden": {
+    name: "Corey Walden",
+    title: "",
+    email: "corey.walden@ecovacservices.com",
+    phone: "(405) 795-7331"
+  },
+  "darrin-winkler": {
+    name: "Darrin Winkler",
+    title: "",
+    email: "darrin.winkler@ecovacservices.com",
+    phone: "(405) 317-2730"
+  },
+
+  "cherie-almeida": {
+    name: "Cherie Almeida",
+    title: "",
+    email: "cherie.almeida@equusenv.com",
+    phone: "(918) 519-0192"
+  },
+  "julie-czech": {
+    name: "Julie Czech",
+    title: "",
+    email: "julie.czech@equusenv.com",
+    phone: "(918) 519-2990"
+  },
+  "sally-douglass": {
+    name: "Sally Douglass",
+    title: "",
+    email: "sally.douglass@equusenv.com",
+    phone: "(918) 409-3922"
+  },
+  "eric-farrar": {
+    name: "Eric Farrar",
+    title: "",
+    email: "eric.farrar@equusenv.com",
+    phone: "(918) 605-4528"
+  },
+  "kyle-fishburn": {
+    name: "Kyle Fishburn",
+    title: "",
+    email: "kyle.fishburn@equusenv.com",
+    phone: "(405) 205-2562"
+  },
+  "terry-fisher": {
+    name: "Terry Fisher",
+    title: "",
+    email: "terry.fisher@equusenv.com",
+    phone: "(918) 344-8895"
+  },
+  "lowell-graves": {
+    name: "Lowell Graves",
+    title: "",
+    email: "lowell.graves@equusenv.com",
+    phone: "(918) 378-8565"
+  },
+  "justin-hoppis": {
+    name: "Justin Hoppis",
+    title: "",
+    email: "justin.hoppis@equusenv.com",
+    phone: "(918) 237-3292"
+  },
+  "sean-hussey": {
+    name: "Sean Hussey",
+    title: "",
+    email: "sean.hussey@equusenv.com",
+    phone: "(918) 282-0868"
+  },
+  "steve-kennedy": {
+    name: "Steve Kennedy",
+    title: "",
+    email: "steve.kennedy@equusenv.com",
+    phone: "(918) 519-0553"
+  },
+  "chris-leger": {
+    name: "Chris Leger",
+    title: "",
+    email: "chris.leger@equusenv.com",
+    phone: "(918) 519-9180"
+  },
+  "bruce-mckenzie": {
+    name: "Bruce McKenzie",
+    title: "AI COO",
+    email: "bruce.mckenzie@equusenv.com",
+    phone: "(918) 906-6780"
+  },
+  "matt-mugavero": {
+    name: "Matt Mugavero",
+    title: "",
+    email: "matthew.mugavero@equusenv.com",
+    phone: "(918) 519-9724"
+  },
+
+  "robert-andersen": {
+    name: "Robby Andersen",
+    title: "Environmental Technician",
+    email: "robert.andersen@stantechservices.com",
+    phone: "(405) 831-8425"
+  },
+  "alisha-booth": {
+    name: "Alisha Booth",
+    title: "",
+    email: "alisha.booth@stantechservices.com",
+    phone: "(405) 727-0715"
+  },
+  "makayla-combs": {
+    name: "Makayla Combs",
+    title: "Environmental Technician",
+    email: "makaylayoung@stantechservices.com",
+    phone: "(405) 953-4710"
+  },
+  "isaac-dale": {
+    name: "Isaac Dale",
+    title: "Geologist",
+    email: "isaac.dale@stantechservices.com",
+    phone: "(405) 534-8534"
+  },
+  "janet-davis": {
+    name: "Janet Davis",
+    title: "Quality Assurance",
+    email: "jdavis@stantechservices.com",
+    phone: "(618) 570-7451"
+  },
+  "michael-ekiss": {
+    name: "Michael Ekiss",
+    title: "ORBCA Team Leader",
+    email: "mekiss@stantechservices.com",
+    phone: "(405) 845-4998"
+  },
+  "gerald-fields": {
+    name: "Gerald Fields",
+    title: "Technician Team Leader",
+    email: "gfields@stantechservices.com",
+    phone: "(405) 205-2566"
+  },
+  "jerry-hansen": {
+    name: "Jerry Hansen",
+    title: "Environmental Technician",
+    email: "jhansen@stantechservices.com",
+    phone: "(405) 205-2567"
+  },
+  "brandon-johnson": {
+    name: "Brandon Johnson",
+    title: "Geologist",
+    email: "brandon.johnson@stantechservices.com",
+    phone: "(405) 205-2565"
+  },
+  "peter-keyes": {
+    name: "Peter Keyes",
+    title: "",
+    email: "peter.keyes@stantechservices.com",
+    phone: "(405) 570-2072"
+  },
+  "robert-lopez": {
+    name: "Robert Lopez",
+    title: "Marketing Manager",
+    email: "rlopez@stantechservices.com",
+    phone: "(405) 669-8457"
+  },
+  "zoie-nelson": {
+    name: "Zoie Nelson",
+    title: "Public Relations",
+    email: "zoie.nelson@stantechservices.com",
+    phone: "(405) 802-7799"
+  },
+  "billy-nievar": {
+    name: "Billy Nievar",
+    title: "Engineer",
+    email: "bnievar@stantechservices.com",
+    phone: "(405) 250-6073"
+  },
+  "rhiannon-poolaw": {
+    name: "Rhiannon Poolaw",
+    title: "Communications Coordinator",
+    email: "rhiannon.poolaw@stantechservices.com",
+    phone: "(580) 771-6020"
+  },
+  "rick-sims": {
+    name: "Rick Sims",
+    title: "Remediation Team Leader",
+    email: "rick.sims@stantechservices.com",
+    phone: "(405) 740-2191"
+  },
+  "rob-williams": {
+    name: "Rob Williams",
+    title: "Investigations Team Leader/Geologist",
+    email: "rwilliams@stantechservices.com",
+    phone: "(405) 249-1806"
+  },
+
   "sky-test-6": {
+    name: "Sky Newell",
     title: "Graphic Designer",
     email: "skyfordnewell@gmail.com",
     phone: "(405) 410-2085"
